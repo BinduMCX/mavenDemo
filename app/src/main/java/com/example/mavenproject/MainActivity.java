b56c6aecd97afaa04762b30197b4c3e94227a2dc
Bindu Madhavi Mishra <bindumadhavi.mishra@freshworks.com>
2023-11-07 20:31:03 +0530

    wip1

--- app/src/main/java/com/example/mavenproject/MainActivity.java
+++ app/src/main/java/com/example/mavenproject/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.mavenproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import com.example.maven_central_demo.MathUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MathUtils.factorial(10);
+
     }
 }
